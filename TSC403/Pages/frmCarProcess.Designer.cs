bc76a538cd26cfe9ccfa98fafd60759df90a0440
Karin <karinking9078loop@gmail.com>
2026-05-15 15:47:09 +0700

    เพิ่มหน้ารถถค้างชั่ง

---
+++ TSC403/Pages/frmCarProcess.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TSC403.Pages
+{
+    partial class frmCarProcess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCarProcess));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mainMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cl_licensePlate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cl_dateIn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cl_weighIn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cl_licensePlate,
+            this.cl_dateIn,
+            this.cl_weighIn});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(664, 470);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainMenuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(687, 29);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mainMenuToolStripMenuItem
+            // 
+            this.mainMenuToolStripMenuItem.Name = "mainMenuToolStripMenuItem";
+            this.mainMenuToolStripMenuItem.Size = new System.Drawing.Size(67, 25);
+            this.mainMenuToolStripMenuItem.Text = "MENU";
+            // 
+            // cl_licensePlate
+            // 
+            this.cl_licensePlate.HeaderText = "ทะเบียนรถ";
+            this.cl_licensePlate.Name = "cl_licensePlate";
+            this.cl_licensePlate.ReadOnly = true;
+            this.cl_licensePlate.Width = 200;
+            // 
+            // cl_dateIn
+            // 
+            this.cl_dateIn.HeaderText = "วันที่ชั่งเข้า";
+            this.cl_dateIn.Name = "cl_dateIn";
+            this.cl_dateIn.ReadOnly = true;
+            this.cl_dateIn.Width = 200;
+            // 
+            // cl_weighIn
+            // 
+            this.cl_weighIn.HeaderText = "น้ำหนักชั่งเข้า";
+            this.cl_weighIn.Name = "cl_weighIn";
+            this.cl_weighIn.ReadOnly = true;
+            this.cl_weighIn.Width = 200;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(578, 523);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 32);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "พิมพ์รายงาน";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // frmCarProcess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(202)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(687, 559);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmCarProcess";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "รถค้างชั่ง";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mainMenuToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cl_licensePlate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cl_dateIn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cl_weighIn;
+        private System.Windows.Forms.Button button3;
+    }
+}
