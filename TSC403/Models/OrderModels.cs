b620d3d979168517491fe59691d0e96251a6c4a2
Karin <karinking9078loop@gmail.com>
2026-05-16 11:10:46 +0700

    add new prop CustomerCode and ProductCode

--- TSC403/Models/OrderModels.cs
+++ TSC403/Models/OrderModels.cs
@@ -12,7 +12,10 @@ namespace TSC403.Models
         public string LicensePlate { get; set; }
         public string OrderNumber { get; set; }
         public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+
         public string CustomerName { get; set; }
+        public string CustomerCode { get; set; }
         public string Note { get; set; }
         public int NetWeight { get; set; }
         public string Status { get; set; }
