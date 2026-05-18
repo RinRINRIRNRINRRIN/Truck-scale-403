2ac0cff34d011ff458d6c335e8252bb676b0fb1b
Karin <karinking9078loop@gmail.com>
2026-05-18 08:29:21 +0700

    Add Product page for Add,Update,Delete

---
+++ TSC403/Pages/frmProduct.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TSC403.Pages
+{
+    partial class frmProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProduct));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cl_code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cl_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(118, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 21);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "ชื่อสินค้า";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(112, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 21);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "รหัสสินค้า";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(197, 287);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(263, 29);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(197, 252);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(263, 29);
+            this.txtCode.TabIndex = 11;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Enabled = false;
+            this.btnEdit.Location = new System.Drawing.Point(12, 340);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(100, 42);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "แก้ไข";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Enabled = false;
+            this.btnDel.Location = new System.Drawing.Point(12, 292);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(100, 42);
+            this.btnDel.TabIndex = 8;
+            this.btnDel.Text = "ลบ";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 244);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 42);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "เพิ่ม";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(8, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "การลบ หรือ แก้ไข ให้ทำการ Double Click ที่ข้อมูลที่ต้องการก่อน ..";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cl_code,
+            this.cl_name});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(448, 205);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDoubleClick);
+            // 
+            // cl_code
+            // 
+            this.cl_code.HeaderText = "รหัสสินค้า";
+            this.cl_code.Name = "cl_code";
+            this.cl_code.ReadOnly = true;
+            // 
+            // cl_name
+            // 
+            this.cl_name.HeaderText = "ชื่อสินค้า";
+            this.cl_name.Name = "cl_name";
+            this.cl_name.ReadOnly = true;
+            this.cl_name.Width = 300;
+            // 
+            // frmProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(477, 389);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmProduct";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ข้อมูลสินค้า";
+            this.Load += new System.EventHandler(this.frmProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cl_code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cl_name;
+    }
+}
---
+++ TSC403/Pages/frmProduct.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TSC403.Db;
+using TSC403.Models;
+
+namespace TSC403.Pages
+{
+    public partial class frmProduct : Form
+    {
+        public frmProduct()
+        {
+            InitializeComponent();
+        }
+
+        void clearForm()
+        {
+            btnAdd.Enabled = true;
+            btnEdit.Enabled = false;
+            btnDel.Enabled = false;
+
+            txtCode.Enabled = true;
+            txtCode.Clear();
+            txtName.Clear();
+        }
+
+        void showDataToGrid()
+        {
+            ProductDb productDb = new ProductDb();
+            List<ProductModels> lists = productDb.SelectAll();
+                dataGridView1.Rows.Clear();
+
+            if (lists != null && lists.Count > 0)
+            {
+                foreach (ProductModels item in lists)
+                    dataGridView1.Rows.Add(item.ProductCode, item.ProductName);
+            }
+        }
+
+        bool checkCodeDuplicate(string data)
+        {
+            foreach (DataGridViewRow rw in dataGridView1.Rows)
+            {
+                if (rw.Cells["cl_code"].Value.ToString() == data)
+                    return true;
+            }
+            return false;
+        }
+
+        private void frmProduct_Load(object sender, EventArgs e)
+        {
+            showDataToGrid();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (checkCodeDuplicate(txtCode.Text))
+            {
+                MessageBox.Show("พบข้อมูลซ้ำกรุณาตรวจสอบใหม่อีกครั้ง", "ข้อมูลซ้ำ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            // Define customer model
+            ProductModels model = new ProductModels
+            {
+                ProductCode = txtCode.Text.Trim(),
+                ProductName = txtName.Text.Trim(),
+            };
+
+            // save new data
+            ProductDb db = new ProductDb();
+            if (!db.AddNew(model))
+            {
+                MessageBox.Show("เกิดข้อผิดผลาดในการบันทึกข้อมูล \nError : " + db.Err, "ข้อผิดผลาดในการบันทึก", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("เพิ่มรายการใหม่สำเร็จ", "เพิ่มรายการ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            clearForm();
+            showDataToGrid();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("คุณต้องการลบข้อมูลสินค้าหรือไม่", "ลบข้อมูล", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                ProductDb db = new ProductDb();
+                if (!db.DeleteByCode(txtCode.Text))
+                {
+                    MessageBox.Show("เกิข้อผิดผลาดในการลบข้อมูลสินค้า", "ลบข้อมูลสินค้า", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                MessageBox.Show("ลบข้อมูลสำเร็จ", "ลบข้อมูล", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                clearForm();
+                showDataToGrid();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            //if (checkCodeDuplicate(txtCode.Text))
+            //{
+            //    MessageBox.Show("พบข้อมูลซ้ำกรุณาตรวจสอบใหม่อีกครั้ง", "ข้อมูลซ้ำ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //    return;
+            //}
+
+            // Define customer model
+            ProductModels model = new ProductModels
+            {
+                ProductCode = txtCode.Text.Trim(),
+                ProductName = txtName.Text.Trim(),
+            };
+
+            // save new data
+            ProductDb db = new ProductDb();
+            if (!db.UpdateByCode(model))
+            {
+                MessageBox.Show("เกิดข้อผิดผลาดในการแก้ไขข้อมูล \nError : " + db.Err, "ข้อผิดผลาดในการแก้ไข", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("แก้ไขรายการสำเร็จ", "แก้ไขรายการ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            clearForm();
+            showDataToGrid();
+        }
+
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            try
+            {
+                string code = dataGridView1.Rows[e.RowIndex].Cells["cl_code"].Value.ToString();
+                string name = dataGridView1.Rows[e.RowIndex].Cells["cl_name"].Value.ToString();
+
+                txtCode.Text = code;
+                txtName.Text = code;
+
+                txtCode.Enabled = false;
+                btnAdd.Enabled = false;
+                btnEdit.Enabled = true;
+                btnDel.Enabled = true;
+            }
+            catch
+            {
+
+
+            }
+        }
+    }
+}
